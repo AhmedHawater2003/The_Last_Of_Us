6fbdb99117992d9ca196da33c482593c3ec55ba1
fashr <fashr@LAPTOP-EV4QJG21>
2023-03-21 13:43:35 +0200

    the failure of zombie was killed

--- src/model/characters/Zombie.java
+++ src/model/characters/Zombie.java
@@ -4,7 +4,7 @@ public class Zombie extends Character {
 static int ZOMBIES_COUNT;
 
 public Zombie() {//constructor to set the name of the zombie and his power
-	super("Zombie"+ZOMBIES_COUNT+1, 40,10);//i used the count +1 to start from 1 not zero
+	super("Zombie "+(ZOMBIES_COUNT+1), 40,10);//i used the count +1 to start from 1 not zero
 	ZOMBIES_COUNT++;
 }
 }
