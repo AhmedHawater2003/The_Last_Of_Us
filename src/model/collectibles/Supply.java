31998ba77c78d34a92c900b96e36785039be7035
ahdmostafa0 <ahdmostafa327@gmail.com>
2023-05-04 23:15:48 +0200

    Fuck you

--- src/model/collectibles/Supply.java
+++ src/model/collectibles/Supply.java
@@ -1,9 +1,23 @@
 package model.collectibles;
 
-public class Supply implements Collectible{
+import model.characters.Hero;
+
+public class Supply implements Collectible {
 
 	public Supply() {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void pickUp(Hero h) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void use(Hero h) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
--- src/model/collectibles/Vaccine.java
+++ src/model/collectibles/Vaccine.java
@@ -1,9 +1,23 @@
 package model.collectibles;
 
-public class Vaccine implements Collectible{
+import model.characters.Hero;
+
+public class Vaccine implements Collectible {
 
 	public Vaccine() {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void pickUp(Hero h) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void use(Hero h) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
